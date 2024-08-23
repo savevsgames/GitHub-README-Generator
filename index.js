7ec16ceba2fe46b18be7bde57e1972307c83515d
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-08-22 22:00:36 -0600

    Add Packages

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 // TODO: Include packages needed for this application
+import colors from "colors";
+import inquirer from "inquirer";
+
+colors.setTheme({
+  silly: "rainbow",
+  input: "grey",
+  verbose: "cyan",
+  prompt: "grey",
+  info: "green",
+  data: "grey",
+  help: "cyan",
+  warn: "yellow",
+  debug: "blue",
+  error: "red",
+});
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "projectName",
+    message: colors.verbose("What is your project's name?"),
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(colors.debug(answers.projectName));
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
